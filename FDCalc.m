b4edd17d0ec398850eec0dbf457a3a60389b48f0
Sunil Simha <Sunil Simha>
2017-02-01 07:56:46 +0530

    initial commit

---
+++ FDCalc.m
@@ -0,0 +1,5 @@
+%% Finite difference function:
+% Created by: H.S. Sunil Simha. Version : 1.0
+% Created on Feb 01 2017
+% This function evaluates the derivative of a given function using Taylor
+% approximation. 
